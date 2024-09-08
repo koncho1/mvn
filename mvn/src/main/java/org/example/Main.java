2a0b173f5f407cd976ecc71716638876742ff1c8
koncho1 <138703432+koncho1@users.noreply.github.com>
2024-09-08 14:56:41 +0200

    Add files via upload

--- mvn/src/main/java/org/example/Main.java
+++ mvn/src/main/java/org/example/Main.java
@@ -1,7 +1,48 @@
 package org.example;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.RegExUtils;
+import java.util.HashMap;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.charset.Charset;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        File textFile=new File("src/main/resources/text.txt");
+        String fileContent=new String();
+        try {
+            fileContent=FileUtils.readFileToString(textFile, (Charset) null);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String noPunct=fileContent.replaceAll("\\p{Punct}","");
+        String[] words=noPunct.split(" ");
+        HashMap<String,Integer> wordCount= new HashMap<String,Integer>();
+        for(String word:words){
+           if (wordCount.containsKey(word)){
+               wordCount.put(word,wordCount.get(word)+1);
+            }
+           else{
+               wordCount.put(word,1);
+           }
+        }
+
+        File wordcount=new File("src/main/resources/wordcount.txt");
+
+
+        for(String word:wordCount.keySet()){
+            try {
+                FileUtils.write(wordcount,word+": "+wordCount.get(word)+"\n",true);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
     }
 }
